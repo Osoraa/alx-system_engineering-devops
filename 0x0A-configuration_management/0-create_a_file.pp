62ba22d59c5704cae43c913cd708e9d57bab898c
Osoraa <sochi98.sn@gmail.com>
2022-10-14 09:19:12 -0700

    Task 0 - Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file {'create_file':
   ensure  => file,
   path    => '/tmp/school',
-  mode    => '0744'
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet'
