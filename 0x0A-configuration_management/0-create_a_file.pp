d5a5e495fcad8c1454354d8598a11e3ffc3c70b3
Osoraa <sochi98.sn@gmail.com>
2022-10-14 09:19:12 -0700

    Task 0 - Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
+# A manifest to create a file with specific content, permissions, ownership and group
+
 file {'create_file':
+  ensure  => file,
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
