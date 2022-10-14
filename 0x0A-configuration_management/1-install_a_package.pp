47b8711ca15516348ad64404e03ece5fdef7c0b6
Osoraa <sochi98.sn@gmail.com>
2022-10-14 21:29:53 +0100

    Task 1 - Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,11 @@
 
 package {'flask':
   ensure   => 2.1.0,
-  provider => pip3
+  provider => pip3,
+  commapd  => '/usr/bin/pip3 install Flask',
 }
+
+# package {'Werkzeug':
+#  ensure   => 2.1.1,
+#  provider => pip3,
+#  }
