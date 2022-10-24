8131a10c8702b194001f1a5a07b0077702510bb7
Osoraa <sochi98.sn@gmail.com>
2022-10-24 12:58:44 +0100

    Advanced Task 5 - Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,3 +3,34 @@
 package {'nginx':
     provider => 'apt',
 }
+
+# Default nginx config file (heredoc)
+$config = @("EOF" /L)
+	server {
+		listen 80 default_server;
+		listen [::]:80 default_server;
+
+		root /var/www/html;
+
+		index.html;
+
+		server_name _;
+		rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+
+		location / {
+			try_files $uri $uri/ =404;
+		}
+	}
+	| -EOF
+
+# Configures nginx with required configurations
+exec {'Config script':
+	path => '/usr/bin'
+	command => "echo '$config' | sudo tee /etc/nginx/sites-enabled/default"
+}
+
+# Default page
+exec {'Default file':
+	path => '/usr/bin',
+	command => "echo 'Hello World!' | sudo tee /var/nginx/html/index.html"
+}
