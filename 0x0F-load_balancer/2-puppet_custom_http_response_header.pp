26a912761479e595e35adfebfa201e130a32732e
Osoraa <sochi98.sn@gmail.com>
2022-10-26 23:37:16 +0100

    Advanced Task 2 - Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,50 +1,10 @@
 # A manifest to install and configure nginx
 
-package {'nginx':
-    provider => 'apt'
-}
+exec { 'run':
+  path    => ['/usr/bin'],
 
-# Default nginx config file (heredoc)
-$config = @(EOF /L)
-	server {
-		listen 80 default_server;
-		listen [::]:80 default_server;
-
-		root /var/www/html;
-
-		index index.html index.nginx-debian.html;
-
-		server_name _;
-		rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-		error_page 404 /404.html;
-		add_header X-Served-By $HOSTNAME;
-
-		location / {
-			try_files $uri $uri/ =404;
-		}
-	}
-	| -EOF
-
-# Default file
-exec {'Default file':
-  path    => '/usr/bin',
-  command => "echo 'Hello World!' | sudo tee /var/www/html/index.html"
-}
-
-# Error file
-exec {'Error file':
-  path    => '/usr/bin',
-  command => "echo Ceci n\\'est pas une page | sudo tee /var/www/html/404.html"
-}
-
-# Configures nginx with required configurations
-exec {'Config script':
-  path    => '/usr/bin',
-  command => "echo '${config}' | sudo tee /etc/nginx/sites-enabled/default"
-}
-
-# Restart nginx
-exec {'Restart nginx':
-	path    => ['/usr/bin', '/usr/sbin'],
-	command => 'service nginx restart'
+  command => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _;/a \\\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default;
+	sudo service nginx restart',
 }
